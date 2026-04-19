e3518ca781846a1b3b42131ae13a9ec4068fa65f
towasi-dot <tomwasi@outlook.com>
2026-04-19 16:27:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+x, y = map(int, input().split())
+words = list(input().split())
+wordSearch = [list(input()) for _ in range(y)]
+# TODO: add diagonal words support
+found = []
+
+for word in words:
+    length = len(word)
+
+    for j in range(y):  # Horizontal (left -> right)
+        for i in range(x - length + 1):
+            row = "".join(wordSearch[j][i:i+length])
+            if row == word or row == word[::-1]:
+                if word not in found:
+                    found.append(word)
+                for k in range(length):
+                    wordSearch[j][i+k] = wordSearch[j][i+k].upper()
+
+    for i in range(x): # Vertical (top → bottom)
+        for j in range(y - length + 1):
+            col = "".join(wordSearch[j+k][i] for k in range(length))
+            if col == word or col == word[::-1]:
+                if word not in found:
+                    found.append(word)
+                for k in range(length):
+                    wordSearch[j+k][i] = wordSearch[j+k][i].upper()
+
+suffix = "these are all" if len(found) == len(words) else "rest are not there"
+print(f"Found words: {' '.join(found)}, {suffix}")
+for row in wordSearch:
+    print("".join(row))
